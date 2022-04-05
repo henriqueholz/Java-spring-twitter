5335d2c4f27449a49befef3178f92a8bc3ba3d7b
henriqueholz <henriqueholz@live.com>
2022-04-04 21:08:57 -0300

    Adding swagger

--- src/main/java/com/henrique/posterr/PosterrApplication.java
+++ src/main/java/com/henrique/posterr/PosterrApplication.java
@@ -8,14 +8,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import com.google.common.base.Predicate;
-import static com.google.common.base.Predicates.or;
-import static springfox.documentation.builders.PathSelectors.regex;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -25,10 +19,20 @@ import java.util.Arrays;
 import java.util.List;
 
 @SpringBootApplication
+@EnableSwagger2
 public class PosterrApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PosterrApplication.class, args);
+	}
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
 	}
 
 	// Adding some mock users and posts to help the testing
